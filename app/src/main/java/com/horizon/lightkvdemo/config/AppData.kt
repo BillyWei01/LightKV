0889e9a3ae1b26da7f3d6da083198a3e7e46d006
John <across_horizon@qq.com>
2018-07-21 15:20:11 +0800

    Update AppData.kt

--- app/src/main/java/com/horizon/lightkvdemo/config/AppData.kt
+++ app/src/main/java/com/horizon/lightkvdemo/config/AppData.kt
@@ -13,17 +13,12 @@ object AppData : KVModel() {
         return LightKV.Builder(GlobalConfig.appContext, "app_data")
                 .logger(AppLogger)
                 .executor(AsyncTask.THREAD_POOL_EXECUTOR)
-                // just for demo，it's unnecessary to define keys if we don't need to print data
-                .keys(Keys::class.java)
                 .encoder(GzipEncoder)
                 .async()
     }
 
     var showCount by int(1)
-
-    // just encode sample, in fact, just long text like json or xml(or else) should be compress
-    var account by string(2 or DataType.ENCODE)
+    var account by string(2)
     var token by string(3)
-
     var secret by array(4 or DataType.ENCODE)
 }
